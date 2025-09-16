180b74316c9dbea3132b45b1f82ce72d20bfc991
Jesus Pacheco <pachecobarrios7@gmail.com>
2025-09-16 14:19:52 -0500

    Ultimas modificaciones

--- flutter_application_1/lib/modelo/contacto.dart
+++ flutter_application_1/lib/modelo/contacto.dart
@@ -1,28 +1,15 @@
 class Contacto {
-  final int id;
   final String nombre;
-  final String email;
-  final String avatarUrl;
-  final String lastMessage;
-  final String lastMessageTime;
+
 
   Contacto({
-    required this.id,
     required this.nombre,
-    required this.email,
-    required this.avatarUrl,
-    required this.lastMessage,
-    required this.lastMessageTime,
+
   });
 
   factory Contacto.fromJson(Map<String, dynamic> json) {
     return Contacto(
-      id: json['id'],
       nombre: json['name'],
-      email: json['email'],
-      avatarUrl: json['avatarUrl'] ?? '', 
-      lastMessage: json['lastMessage'] ?? '',
-      lastMessageTime: json['lastMessageTime'] ?? '',
     );
   }
 }
